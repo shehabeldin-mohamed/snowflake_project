ca81fb2516d0350113efa6fc5ced276d112d1a96
shehabeldin-mohamed <shehabkh249@gmail.com>
2026-04-06 15:48:56 +0200

    Added Snowflake folder which contains ddl scripts,time travel, and procedures

---
+++ snowflake/silver_layer/ddl.sql
@@ -0,0 +1,23 @@
+USE DATABASE AIRLINE_EDW;
+USE SCHEMA AIRLINE_EDW.SILVER;
+
+
+CREATE OR REPLACE TABLE CLEAN(
+    passenger_id VARCHAR,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    gender VARCHAR,
+    age INT,
+    nationality VARCHAR,
+    airport_name VARCHAR,
+    airport_country_code VARCHAR,
+    country_name VARCHAR,
+    airport_continent STRING,
+    continents VARCHAR,
+    departure_date DATE,
+    arrival_airport VARCHAR,
+    pilot_name VARCHAR,
+    flight_status VARCHAR,
+    ticket_type VARCHAR,
+    passenger_status VARCHAR
+);
